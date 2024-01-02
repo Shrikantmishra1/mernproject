1bd6659c7739af228be157869cdcac17f70450e3
shrikant mishra <mishrashri2001@gmail.com>
2024-01-02 16:53:49 +0530

    Add backend to the our app and listen to 5001

---
+++ api/index.js
@@ -0,0 +1,7 @@
+import { log } from "console";
+import express from "express";
+const app=express();
+
+ app.listen(5002,()=>{
+    console.log("listen to 5001 port")  
+ });
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
-     <div className=' shadow-lg bg-three '>
-          <div className='flex justify-between items-center max-w-6xl mx-auto p-3 '>
-               <Link to="/">
-               <h1 className='font-bold'> MERN APP</h1>
-               </Link>
-              <ul className='flex gap-4'>
-                 <Link to='/'> <li>Home</li></Link>
-                 <Link to='/about'> <li>About</li></Link>
-                 <Link to='/sign-in'> <li>sign In</li></Link>
-              </ul>
-          </div>
-     </div>
-  )
-}
+    <div className=" shadow-lg bg-three ">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3 ">
+        <Link to="/">
+          <h1 className="font-bold"> MERN APP</h1>
+        </Link>
+        <ul className="flex gap-4">
+          <Link to="/">
+            {" "}
+            <li className="hover:bg-four">Home</li>
+          </Link>
+          <Link to="/about">
+            {" "}
+            <li className="hover:bg-four">About</li>
+          </Link>
+          <Link to="/sign-in">
+            {" "}
+            <li className="hover:bg-four" >sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;

8cac87754ac093e3f7a9aed5585c936bd58fed4a
shrikant mishra <mishrashri2001@gmail.com>
2024-01-02 19:20:36 +0530

    created api routes for the testing

--- api/index.js
+++ api/index.js
@@ -2,12 +2,13 @@ import { log } from "console";
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 // mongoose.connect(
 //   "mongodb+srv://mishrashri2001:root@mern.hrqva.mongodb.net/?mern=true&w=majority"
 // );
 
 dotenv.config();
-console.log(process.env.MONGO);
+// console.log(process.env.MONGO);
 mongoose.connect(process.env.MONGO).then(()=>{
       console.log("connetcted");
 }).catch((err)=>{
@@ -16,5 +17,12 @@ mongoose.connect(process.env.MONGO).then(()=>{
 const app = express();
 
  app.listen(5002, () => {
-      console.log("listen to 5001 port");
+      console.log("listen to 5002 port");
 });
+//Create rout:
+//   app.get(('/'),(req,res)=>{
+//       res.json({
+//           message:"Api is working correctly"
+//       })
+//   })
+app.use("/api/user",userRoutes);

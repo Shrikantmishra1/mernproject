53328e5ec41cc3fa079d5adab37f917c79d0f836
shrikant mishra <mishrashri2001@gmail.com>
2024-01-02 16:33:43 +0530

    Create Header Compoents

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,18 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'three': '#c4b5fd',
+      'four': '#a78bfa',
+      'five': '#8b5cf6',
+      'six': '#7c3aed',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
   },
   plugins: [],
 }
